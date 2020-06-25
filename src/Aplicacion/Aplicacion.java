c16be1a5d6b16c36291fe7f851fb677585b472f7
Bairon Dajome <Bairon Dajome@Equipo_Bairon>
2020-06-25 13:08:06 -0500

    trabaje en la clase cliente deje listo el boton actualizar y el boton eliinar

--- src/Aplicacion/Aplicacion.java
+++ src/Aplicacion/Aplicacion.java
@@ -6,5 +6,6 @@ import Controlador.Control;
 public class Aplicacion {
     public static void main(String[] args) {       
         Control app=new Control();
+        System.out.println("weriewoi");
     }
 }
